38335f9d98052ec83f1b590b2edae9eda5213e6c
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-08-30 20:59:35 +0545

    done 1.1

---
+++ part 1/course-info/src/Header.jsx
@@ -0,0 +1,13 @@
+import { PropTypes } from 'prop-types'
+
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+Header.propTypes = {
+    course: PropTypes.string.isRequired,
+};
+
+export default Header
